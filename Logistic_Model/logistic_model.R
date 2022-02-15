f1a5e830402617bf6cc85db13c1f20b06baa6c61
eckertal <anna-lena.eckert@charite.de>
2022-02-15 11:45:19 +0100

    update visual patsy to account for old block seq

--- Logistic_Model/logistic_model.R
+++ Logistic_Model/logistic_model.R
@@ -12,7 +12,7 @@ data_vis <- read.csv("C://Users//annae//Desktop//ChoiceHistory_Psych//Data//Exp2
 
 # set up model on auditory data...
 # patsy auditory
-patsy_aud <- 'response ~ (1|sbj_id) + target_z + stimulus_z + (1|block:sbj_id) + cue_z * PPS_z + stim_1_z + resp_1_z * PPS_z * block_type_z + cue_z*resp_1_z*PPS_z'
+patsy_aud <- 'response ~ (1|sbj_id) + target_z + stimulus_z + (1|block:sbj_id) + cue_z * PPS_z * block_type + stim_1_z + resp_1_z * PPS_z * block_type_z + cue_z*resp_1_z*PPS_z'
 
 # patsy for auditory neutral blocks only
 #patsy_aud <- 'response_z ~ (1|sbj_id) + target_z + stimulus_z + cue_z * PPS_z + stim_1_z + resp_1_z * PPS_z'
@@ -28,7 +28,7 @@ sink()
 
 # set up model on visual data... 
 # patsy visual
-patsy_vis <- 'response ~ (1|sbj_id) + target_z + coherence_z + (1|Block:sbj_id) + cue_z * PPS_z + stim_1_z + resp_1_z * PPS_z * block_type_z + cue_z*resp_1_z*PPS_z'
+patsy_vis <- 'response ~ (1|sbj_id) + target_z + coherence_z + (1|Block:sbj_id) + cue_z * PPS_z * block_type + stim_1_z + resp_1_z * PPS_z * block_type_z + cue_z*resp_1_z*PPS_z'
 
 # patsy visual, neutral blocks only
 #patsy_vis <- 'response ~ (1|sbj_id) + target + coherence + cue * PPS_z + stim_1 + resp_1 * PPS_z'
